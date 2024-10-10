79c9f007718719da66872ef851aa14085e95c715
FABRINET\jakkapanp <jakkapanp@fabrinet.co.th>
2024-10-10 07:46:43 +0700

    reduce img size

--- src/components/ClientExamples/ClientExamples.js
+++ src/components/ClientExamples/ClientExamples.js
@@ -1,69 +1,3 @@
-// import React from "react";
-// import { Box, Container, Typography, Grid, CardMedia } from "@mui/material";
-
-// const ClientExamples = () => {
-//   const clients = [
-//     {
-//       name: "SCG",
-//       description: "บริการพัฒนาแบบเว็บไซต์",
-//       logo: "images/customers/scg-logo.png",
-//     },
-//     {
-//       name: "Exotic Kingdoms",
-//       description: "บริการออกแบบและพัฒนาเว็บไซต์",
-//       logo: "images/customers/exotic-kingdoms.png",
-//     },
-//   ];
-
-//   return (
-//     <Box id="clients" sx={{ py: 10, backgroundColor: "#f5f5f5" }}>
-//       <Container maxWidth="lg">
-//         <Typography variant="h4" gutterBottom align="center">
-//           ลูกค้าที่ใช้บริการของเรา
-//         </Typography>
-//         <Grid
-//           container
-//           spacing={2}
-//           sx={{
-//             alignItems: "center",
-//             display: "flex",
-//             justifyContent: "center",
-//           }}
-//         >
-//           {clients.map((client, index) => {
-//             let aosEffect;
-//             if (index % 2 === 0) {
-//               aosEffect = "fade-right";
-//             } else {
-//               aosEffect = "fade-left";
-//             }
-
-//             return (
-//               <Grid item xs={12} sm={6} md={4} key={index}>
-//                 <img
-//                   data-aos={aosEffect}
-//                   data-aos-duration="600"
-//                   src={client.logo}
-//                   style={{
-//                     width: "250px",
-//                     cursor: "pointer",
-//                     transition: "transform 0.2s, box-shadow 0.2s",
-//                     ":hover": {
-//                       transform: "scale(1.02)",
-//                     },
-//                   }}
-//                 ></img>
-//               </Grid>
-//             );
-//           })}
-//         </Grid>
-//       </Container>
-//     </Box>
-//   );
-// };
-
-// export default ClientExamples;
-
 import React from "react";
 import { Box, Container, Typography, Grid, CardMedia } from "@mui/material";
 
@@ -142,7 +76,7 @@ const ClientExamples = () => {
                       transform: "scale(1.05)",
                       boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)",
                     },
-                    maxWidth: { xs: "180px", sm: "220px" },
+                    maxWidth: { xs: "120px", sm: "220px" },
                   }}
                 >
                   <CardMedia
